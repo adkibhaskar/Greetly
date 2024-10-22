fe939db250f6f8fcc74c95d0358500bfd8baa687
Bhaskar Adki <adkibhaskar2002@gmail.com>
2024-10-22 11:22:03 +0530

    Modify the port in Dockerfile

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const app=express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 
 app.listen(port,()=>{
     console.log("Server started Successfully!");
